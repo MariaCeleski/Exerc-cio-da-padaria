5353e4ac4c0f62dc4aff3e2c41337ca65a5870f7
Maria de Lourdes Celeski <marialceleski@gmail.com>
2022-02-08 10:21:55 -0300

    Exercícios Bootcamp Talk'n Talk - The bakery - Aula 5 - C#

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Exercício_da_padaria
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite seu nome: ");
+            String name = Console.ReadLine();
+            Console.WriteLine($"Olá {name}!");
+            
+            Console.Write("Digite o produto que gostaria de comprar: ");
+            String product = Console.ReadLine();
+            Console.WriteLine($"Voce quer comprar {product}!");
+            
+            Console.Write($"Digite a quantidade de {product} que gostaria de comprar: ");
+            float quantity = int.Parse(Console.ReadLine());
+            Console.WriteLine($"Voce gostaria de comprar {quantity} unidades de {product}!");
+
+            Console.Write($"Digite o valor do {product} que gostaria de comprar: ");
+            float value = int.Parse(Console.ReadLine());
+            float some1 = (quantity * value);
+
+            string value1 = $"Voce vai pagar o valor de ${some1} por {quantity} unidades de {product}!";
+            Console.WriteLine(value1);
+
+        }
+    }
+}
